af6bd066cb1e39db45b08840205a128fcfafbc5a
Spencer Evans <spencevans@gmail.com>
2017-08-18 23:45:27 +0000

    UsersController importes from last version

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+require 'pry'
+require 'rack-flash'
+class UsersController < ApplicationController
+  use Rack::Flash
+
+    get "/users" do
+      if logged_in?
+        @users = User.all
+        erb :'/users/index'
+      else
+        flash[:message] = "Please login to view this page."
+        redirect "/login"
+      end
+    end
+
+    get "/signup" do
+      erb :'/users/new'
+    end
+
+    post "/signup" do
+      @user = User.create(params[:user])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    end
+
+    get '/users/:id' do
+
+      if logged_in? && current_user_logged_in?
+        @user = current_user
+        erb :'users/show'
+      else
+        flash[:message] = "Private user's page.  Please log in as user to view"
+        redirect "/login"
+      end
+    end
+end
