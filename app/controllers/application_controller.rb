ab24d26ef11c857e063c7196ed0be9dac67d523b
Spencer Evans <spencevans@gmail.com>
2017-08-19 00:04:39 +0000

    sign-up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,26 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "my_application_secret"
+  set :views, 'app/views'
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
 
-  get "/" do
+  get '/' do
     erb :welcome
   end
 
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def current_user_logged_in?
+      current_user.id == session[:user_id]
+    end
+  end
 end
