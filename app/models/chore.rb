33705514ff6ca125a445b54a10ab2b3ff2b5187b
Spencer Evans <spencevans@gmail.com>
2017-08-19 04:04:56 +0000

    sluggified chore.rb

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,4 +3,20 @@ class Chore < ActiveRecord::Base
   has_many :tasks, through: :tasks
   belongs_to :user
 
+  validates :title, presence: true
+
+  def self.find_by_slug(slugged)
+    found = []
+    self.all.each do |obj|
+      if slugged == obj.slug
+        found << obj
+      end
+    end
+    found[0]
+  end
+
+  def slug
+    strip_chars_array = self.title.downcase.scan(/[a-z0-9]+/)
+    slug = strip_chars_array.join("-")
+  end
 end
